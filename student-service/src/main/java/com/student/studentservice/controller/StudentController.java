187e905a1183f35e6d2a0bc4c8cd14b2469a685e
Chanel Boris <chanelboris@gmail.com>
2021-08-25 17:55:36 +0200

    created class StudentController added annotations and the method getAll

---
+++ student-service/src/main/java/com/student/studentservice/controller/StudentController.java
@@ -0,0 +1,26 @@
+package com.student.studentservice.controller;
+
+import com.student.studentservice.model.Student;
+import com.student.studentservice.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
+
+/**
+ * mapping all request to "/student"
+ */
+@RestController
+@RequestMapping("/students")
+public class StudentController {
+    
+    @Autowired
+    StudentRepository repo;
+
+    @GetMapping
+    Collection<Student> getAll() {
+        return repo.findAll();
+    }
+}
