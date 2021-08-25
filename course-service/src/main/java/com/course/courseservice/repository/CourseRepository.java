75c496a3d6f83e9f5f76cd2737ed87ebecf6806e
Chanel Boris <chanelboris@gmail.com>
2021-08-25 20:54:29 +0200

    Created package and  class CourseRepository

---
+++ course-service/src/main/java/com/course/courseservice/repository/CourseRepository.java
@@ -0,0 +1,8 @@
+package com.course.courseservice.repository;
+
+import com.course.courseservice.model.Course;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CourseRepository extends JpaRepository<Course, Integer> {
+    
+}
