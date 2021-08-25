862b3f1eb37f9ffd5c49d2d5f71e4756f40a29c2
Chanel Boris <chanelboris@gmail.com>
2021-08-25 18:06:10 +0200

    created data.sql which is the database file

---
+++ student-service/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into student (id, name, surname, mail) values
+(1, 'Chanel', 'bob', 'bob@gmail.com'),
+(2, 'mira', 'mimi', 'mimi@gmail.com'),
+(3, 'enzo', 'mi', 'zomi@gmail.com'),
+(4, 'anel', 'coco', 'coco@gmail.com'),
+(5, 'Dany', 'foz', 'danyfoz@gmail.com');
