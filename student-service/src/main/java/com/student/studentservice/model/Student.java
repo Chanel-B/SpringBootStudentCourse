63452fe610281a98a996cc55cf944362e81856f4
Chanel Boris <chanelboris@gmail.com>
2021-08-25 17:47:29 +0200

    created class student in model
    with addition of annotations

---
+++ student-service/src/main/java/com/student/studentservice/model/Student.java
@@ -0,0 +1,28 @@
+package com.student.studentservice.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ * lombok viene usato qua
+ */
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private String surname;
+    private String mail;
+    
+}
