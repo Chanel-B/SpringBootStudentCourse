0e013fd05b7092ff02ee562bfe355bdff078a354
Chanel Boris <chanelboris@gmail.com>
2021-08-25 18:51:55 +0200

    generated the course service from spring initializr

--- student-service/src/main/java/com/student/studentservice/controller/StudentController.java
+++ student-service/src/main/java/com/student/studentservice/controller/StudentController.java
@@ -4,10 +4,12 @@ import com.student.studentservice.model.Student;
 import com.student.studentservice.repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Collection;
+import java.util.Optional;
 
 /**
  * mapping all request to "/student"
@@ -22,5 +24,10 @@ public class StudentController {
     @GetMapping
     Collection<Student> getAll() {
         return repo.findAll();
+    }
+
+    @GetMapping("/{id}")
+    Optional<Student> getStudentById(@PathVariable("id") int id){
+        return repo.findById(id);
     }
 }
